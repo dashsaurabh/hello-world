c969423f6e5bc98e55f2e7f1bcaf5d863296ea7e
Saurabh <dashsaurabh@gmail.com>
2018-02-25 17:15:54 +0530

    Added Thymeleaf view for books

--- src/main/java/com/oyster/product/demo/bootstrap/DevBootstrap.java
+++ src/main/java/com/oyster/product/demo/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.oyster.product.demo.bootstrap;
 
 import com.oyster.product.demo.model.Author;
 import com.oyster.product.demo.model.Book;
+import com.oyster.product.demo.model.Publisher;
 import com.oyster.product.demo.repositories.AuthorRepository;
 import com.oyster.product.demo.repositories.BookRepository;
+import com.oyster.product.demo.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,16 +15,22 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository= publisherRepository;
     }
 
     private void initData(){
 
+        Publisher publisher = new Publisher();
+        publisher.setName("Harper Collins");
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric","Evans");
-        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        Book ddd = new Book("Domain Driven Design","1234",publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -30,7 +38,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod","Johnson");
-        Book noEjb = new Book("J2EE development without EJB","23444","Worx");
+        Book noEjb = new Book("J2EE development without EJB","23444",publisher);
         rod.getBooks().add(noEjb);
 
         authorRepository.save(rod);
